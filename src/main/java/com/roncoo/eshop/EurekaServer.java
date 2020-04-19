ed71ed756c60350a81ce8de122bef3c0fe2a3a26
陈思男 <1549770917@qq.com>
2020-04-19 16:52:30 +0800

    no message

---
+++ src/main/java/com/roncoo/eshop/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.roncoo.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description:
+ *
+ * @author 陈思男
+ * @date 2020年04月13日 19:27
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
